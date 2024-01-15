07973154772466b6b772d8b1eafb5086f0ac3c9e
splatos <92713263+Splatoscar@users.noreply.github.com>
2024-01-15 18:07:11 +0100

    Create armes.js

---
+++ armes.js
@@ -0,0 +1,24 @@
+// data.js
+const imagesData1 = [
+    { name: "Image1_1", url: "image/discord.png" },
+    { name: "Image1_2", url: "image/Determination.png" },
+    // Ajoute d'autres données d'images au besoin
+];
+
+const imagesData2 = [
+    { name: "Image2_1", url: "image/discord.png" },
+    { name: "Image2_2", url: "image/Determination.png" },
+    // Ajoute d'autres données d'images au besoin
+];
+
+const imagesData3 = [
+    { name: "Image3_1", url: "image/discord.png" },
+    { name: "Image3_2", url: "image/Determination.png" },
+    // Ajoute d'autres données d'images au besoin
+];
+
+function getRandomImage(imagesData) {
+    return imagesData[Math.floor(Math.random() * imagesData.length)];
+}
+
+export { imagesData1, imagesData2, imagesData3, getRandomImage };
