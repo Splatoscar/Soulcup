2ad75d2c079e551da849c5fdec3d1d361c87c61c
splatos <92713263+Splatoscar@users.noreply.github.com>
2024-01-15 18:06:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -79,29 +79,3 @@ function updateTank() {
 
 // Appeler la fonction d'initialisation
 updateTank();
-
-
-// data.js
-const imagesData1 = [
-    { name: "Image1_1", url: "image/discord.png" },
-    { name: "Image1_2", url: "image/Determination.png" },
-    // Ajoute d'autres données d'images au besoin
-];
-
-const imagesData2 = [
-    { name: "Image2_1", url: "image/discord.png" },
-    { name: "Image2_2", url: "image/Determination.png" },
-    // Ajoute d'autres données d'images au besoin
-];
-
-const imagesData3 = [
-    { name: "Image3_1", url: "image/discord.png" },
-    { name: "Image3_2", url: "image/Determination.png" },
-    // Ajoute d'autres données d'images au besoin
-];
-
-function getRandomImage(imagesData) {
-    return imagesData[Math.floor(Math.random() * imagesData.length)];
-}
-
-export { imagesData1, imagesData2, imagesData3, getRandomImage };
